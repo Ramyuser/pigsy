099ed31058c02535963676b4afd997b2470fa75c
Ramyuser <89673479+Ramyuser@users.noreply.github.com>
2022-07-14 19:48:36 -0700

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,19 @@
+function tv() {
+    window.open("tv.html");
+}
+
+function comuter() {
+    window.open("computer.html");
+}
+
+function bed() {
+    window.open("bed.html");
+}
+
+function small() {
+    window.open("small.html");
+}
+
+function sw() {
+    window.open("sw.html");
+}
